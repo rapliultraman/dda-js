27061b3ce88fc5d228a52ffaa2fa4b4f699daaa6
luminostar <rafialayyubi08@gmail.com>
2023-12-24 07:46:36 +0800

    projectile motion logic

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Initialize
+const initialVelocityInput = document.getElementById("velocity");
+const outputVelocity = document.getElementById("vOutput");
+outputVelocity.innerHTML = initialVelocityInput.value;
 
-setupCounter(document.querySelector('#counter'))
+initialVelocityInput.oninput = function() {
+  outputVelocity.innerHTML = this.value;
+}
+
+const initialAngleInput = document.getElementById("angel");
+const outputAngle = document.getElementById("aOutput");
+outputAngle.innerHTML = initialAngleInput.value;
+
+initialAngleInput.oninput = function() {
+  outputAngle.innerHTML = this.value;
+}
+
+const initialGravityInput = document.getElementById("gravity");
+const outputGravity = document.getElementById("gOutput");
+outputGravity.innerHTML = initialGravityInput.value;
+
+initialGravityInput.oninput = function() {
+  outputGravity.innerHTML = this.value;
+}
+
+let x = 0;
+let y = canvas.height;
+let time = 0;
+
+function drawProjectile() {
+  const initialVelocity = parseFloat(initialVelocityInput.value);
+  const initialAngle = (parseFloat(initialAngleInput.value) / 180) * Math.PI;
+  const initialGravity = parseFloat(initialVelocityInput.value);
+  
+  const vx = initialVelocity * Math.cos(initialAngle);
+  const vy = initialVelocity * Math.sin(initialAngle) - initialGravity * time;
+
+  x += vx;
+  y -= vy;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fill();
+
+  if (y > canvas.height || x > canvas.width) {
+    clearInterval(interval);
+  }
+
+  time += 0.1;
+}
+
+const interval = setInterval(drawProjectile, 50);
+
